6788d81fd927158ff8f78af636ce710329310f62
LazyTong <lt2009@inventati.org>
2024-07-15 17:35:40 +0800

    Update for top article support

--- components/houseList.vue
+++ components/houseList.vue
@@ -1,5 +1,8 @@
 <template>
   <el-space wrap fill>
+    <template v-for="article in top" :key="article._path">
+      <houseTopCard :article="article" />
+    </template>
     <template v-for="article in posts" :key="article._path">
       <houseCard :article="article" />
     </template>
@@ -7,4 +10,8 @@
 </template>
 <script setup>
   const { posts } = defineProps(['posts']);
+  const { top } = queryContent('posts').sort({
+    date: -1,
+    top: true
+  });
 </script>
