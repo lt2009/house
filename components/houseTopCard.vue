b492c8fa11133698b8b0db8551ee23698b4d7eec
LazyTong <lt2009@inventati.org>
2024-07-15 17:41:14 +0800

    Create the component of support for top article

---
+++ components/houseTopCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-link :underline="false" :href="props.article._path">
+    <el-card :header="title" :footer="format(parseISO(props.article.date), 'yyyy年MM月dd日')">
+      <el-text>{{ props.article.description }}</el-text>
+    </el-card>
+  </el-link>
+</template>
+<script setup>
+  import { format, parseISO } from 'date-fns'
+  const title = '[置顶] '+props.article.title;
+  const props = defineProps(['article']);
+</script>
